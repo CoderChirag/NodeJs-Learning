934ba8b99537a4806e40fd554e987936a648ca10
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-05-09 15:51:21 +0530

    Implement Pug

--- My Shop/app.js
+++ My Shop/app.js
@@ -5,17 +5,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+	res.status(404).render('404', { pageTitle: 'Page not found' });
 });
 
 app.listen(3000);
--- My Shop/routes/shop.js
+++ My Shop/routes/shop.js
@@ -3,11 +3,13 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+	const products = adminData.products;
+	res.render('shop', { prods: products, pageTitle: 'My Shop', path: '/' });
 });
 
 module.exports = router;
