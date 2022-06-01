c5a6df9720d61211adac80e8c3b0d2a4316340ef
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-06-01 16:18:53 +0530

    Setup express-session

--- My Shop/app.js
+++ My Shop/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -18,6 +19,9 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	session({ secret: 'my secret', resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
 	User.findById('6295cfb0ba55eb1597517dec')
