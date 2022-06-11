3de9b04480bd503fd46ed7acbdf73a9f06037344
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-06-11 15:51:07 +0530

    Hash Passwords

--- My Shop/controllers/auth.js
+++ My Shop/controllers/auth.js
@@ -1,12 +1,22 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 /** @type {import("express").RequestHandler} */
 exports.getLogin = (req, res, next) => {
-	console.log(req.session);
 	if (req.session.isAuthenticated) return res.redirect('/');
 	res.render('auth/login', {
 		path: '/login',
 		pageTitle: 'Login',
+		isAuthenticated: req.session.isAuthenticated,
+	});
+};
+
+/** @type {import("express").RequestHandler} */
+exports.getSignup = (req, res, next) => {
+	res.render('auth/signup', {
+		path: '/signup',
+		pageTitle: 'Signup',
 		isAuthenticated: req.session.isAuthenticated,
 	});
 };
@@ -28,27 +38,62 @@ exports.getLogout = (req, res, next) => {
 /** @type {import("express").RequestHandler} */
 exports.postLogin = (req, res, next) => {
 	if (req.session.isAuthenticated) return res.redirect('/');
-	return User.findOne()
+	const email = req.body.email;
+	const password = req.body.password;
+	return User.findOne({ email })
 		.then(user => {
 			if (!user) {
-				const user = new User({
-					name: 'coder',
-					email: 'coder@test.com',
-					cart: {
-						items: [],
-					},
-				});
-				return user.save();
+				return res.redirect('/login');
 			}
-			return user;
+			bcrypt
+				.compare(password, user.password)
+				.then(result => {
+					if (result) {
+						req.session.user = user;
+						req.session.isAuthenticated = true;
+						return req.session.save(err => {
+							if (err) console.log(err);
+							res.redirect('/');
+						});
+					}
+					res.redirect('/login');
+				})
+				.catch(err => {
+					console.log(err);
+				});
 		})
+		.catch(err => {
+			console.log(err);
+		});
+};
+
+/** @type {import("express").RequestHandler} */
+exports.postSignup = (req, res, next) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	const confirmPassword = req.body.confirmPassword;
+
+	User.findOne({ email })
 		.then(user => {
-			req.session.user = user;
-			req.session.isAuthenticated = true;
-			req.session.save(err => {
-				if (err) console.log(err);
-				return res.redirect('/');
-			});
+			if (user) {
+				return res.redirect('/signup');
+			}
+			return bcrypt
+				.hash(password, 12)
+				.then(hashedPwd => {
+					const newUser = new User({
+						email,
+						password: hashedPwd,
+						cart: { items: [] },
+					});
+					return newUser.save();
+				})
+				.then(newUser => {
+					return res.redirect('/login');
+				})
+				.catch(err => {
+					console.log(err);
+				});
 		})
 		.catch(err => {
 			console.log(err);
--- My Shop/routes/auth.js
+++ My Shop/routes/auth.js
@@ -6,8 +6,12 @@ const router = express.Router();
 
 router.get('/login', authController.getLogin);
 
-router.post('/login', authController.postLogin);
+router.get('/signup', authController.getSignup);
 
 router.get('/logout', authController.getLogout);
+
+router.post('/signup', authController.postSignup);
+
+router.post('/login', authController.postLogin);
 
 module.exports = router;
