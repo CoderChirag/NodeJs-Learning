d7681d8b13e4291e341ec0ea781d4301db28517c
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-06-13 14:41:27 +0530

    Add reset Password Token Functionality

--- My Shop/controllers/auth.js
+++ My Shop/controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
@@ -56,6 +57,50 @@ exports.getLogout = (req, res, next) => {
 	} else {
 		return res.redirect('/');
 	}
+};
+
+/** @type {import("express").RequestHandler} */
+exports.getReset = (req, res, next) => {
+	if (req.session.isAuthenticated) return res.redirect('/');
+	let message = req.flash('error');
+	if (message.length > 0) {
+		message = message[0];
+	} else {
+		message = null;
+	}
+	res.render('auth/reset', {
+		path: '/auth/reset',
+		pageTitle: 'Reset Password',
+		errorMessage: message,
+	});
+};
+
+/** @type {import("express").RequestHandler} */
+exports.getUpdatePassword = (req, res, next) => {
+	if (req.session.isAuthenticated) return res.redirect('/');
+	const token = req.params.token;
+	User.findOne({
+		resetToken: token,
+		resetTokenExpiration: { $gt: Date.now() },
+	}).then(user => {
+		if (!user) {
+			req.flash('error', 'Invalid Token');
+			return res.redirect('/auth/reset');
+		}
+		let message = req.flash('error');
+		if (message.length > 0) {
+			message = message[0];
+		} else {
+			message = null;
+		}
+		res.render('auth/update-password', {
+			path: '/auth/update-password',
+			pageTitle: 'Update Password',
+			errorMessage: message,
+			userId: user._id.toString(),
+			passwordToken: token,
+		});
+	});
 };
 
 /** @type {import("express").RequestHandler} */
@@ -133,4 +178,79 @@ exports.postSignup = (req, res, next) => {
 		.catch(err => {
 			console.log(err);
 		});
+};
+
+/** @type {import("express").RequestHandler} */
+exports.postReset = (req, res, next) => {
+	crypto.randomBytes(32, (err, buffer) => {
+		if (err) {
+			console.log(err);
+			return res.redirect('/auth/reset');
+		}
+		const token = buffer.toString('hex');
+		User.findOne({ email: req.body.email })
+			.then(user => {
+				if (!user) {
+					req.flash('error', 'No account with that email found');
+					return null;
+				}
+				user.resetToken = token;
+				user.resetTokenExpiration = Date.now() + 1000 * 60 * 60; // 1 hour from now
+				return user.save();
+			})
+			.then(result => {
+				if (!result) {
+					return res.redirect('/auth/reset');
+				}
+				res.redirect('/');
+				transporter
+					.sendMail({
+						to: req.body.email,
+						from: 'jain.chirag0174@gmail.com',
+						subject: 'Password Reset',
+						html: `
+                <p>You requested a password reset</p>
+                <p>Click this <a href="http://localhost:3000/auth/reset/${token}">link</a> to set a new password</p>
+                `,
+					})
+					.then(result => {
+						console.log('Email Sent');
+					})
+					.catch(err => console.log(err));
+			})
+			.catch(err => console.log(err));
+	});
+};
+
+/** @type {import("express").RequestHandler} */
+exports.postUpdatePassword = (req, res, next) => {
+	const userId = req.body.userId;
+	const newPassword = req.body.password;
+	const passwordToken = req.body.passwordToken;
+	User.findOne({
+		resetToken: passwordToken,
+		resetTokenExpiration: { $gt: Date.now() },
+		_id: userId,
+	})
+		.then(user => {
+			if (!user) {
+				req.flash(
+					'error',
+					'Password reset token is invalid or has expired'
+				);
+				return res.redirect('/auth/reset');
+			}
+			bcrypt
+				.hash(newPassword, 12)
+				.then(hashedPwd => {
+					user.password = hashedPwd;
+					user.resetToken = undefined;
+					user.resetTokenExpiration = undefined;
+					return user.save();
+				})
+				.then(result => {
+					return res.redirect('/auth/login');
+				});
+		})
+		.catch(err => console.log(err));
 };
--- My Shop/routes/auth.js
+++ My Shop/routes/auth.js
@@ -10,8 +10,16 @@ router.get('/signup', authController.getSignup);
 
 router.get('/logout', authController.getLogout);
 
+router.get('/reset', authController.getReset);
+
+router.get('/reset/:token', authController.getUpdatePassword);
+
 router.post('/signup', authController.postSignup);
 
 router.post('/login', authController.postLogin);
+
+router.post('/reset', authController.postReset);
+
+router.post('/update-password', authController.postUpdatePassword);
 
 module.exports = router;
