79bb3fd77c9d1fda39d57393c68eb6184adafa73
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-06-21 19:58:35 +0530

    Set CORS Headers

--- Rest API/app.js
+++ Rest API/app.js
@@ -7,6 +7,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, PATCH, DELETE'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Content-Type, Authorization'
+	);
+	next();
+});
+
 app.use('/api/feed', feedRoutes);
 
 const PORT = process.env.PORT || 8080;
