7c590dce90eb7c29498cd9474ae992e5633046b4
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-06-23 12:18:14 +0530

    Http Status Codes Cheatsheet

--- Rest API/app.js
+++ Rest API/app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader(
 		'Access-Control-Allow-Methods',
-		'GET, POST, PUT, PATCH, DELETE'
+		'GET, POST, PUT, PATCH, DELETE, OPTIONS'
 	);
 	res.setHeader(
 		'Access-Control-Allow-Headers',
