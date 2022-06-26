dfb3aab2ff87241cc7afdc9d147ab58617196130
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-06-26 18:00:18 +0530

    ynchronizing POST additions

--- Rest API - Social Media Backend/controllers/feed.js
+++ Rest API - Social Media Backend/controllers/feed.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const { validationResult } = require('express-validator');
 
+const io = require('../socket');
 const User = require('../models/user');
 const Post = require('../models/post');
 
@@ -61,7 +62,7 @@ exports.getPost = (req, res, next) => {
 };
 
 /** @type {import('express').RequestHandler} */
-exports.createPost = (req, res, next) => {
+exports.createPost = async (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		const error = new Error(
@@ -78,7 +79,6 @@ exports.createPost = (req, res, next) => {
 	const imageUrl = req.file.path.replace(/\\/g, '/');
 	const title = req.body.title;
 	const content = req.body.content;
-	let creator;
 	// Create post in db
 	const post = new Post({
 		title,
@@ -86,29 +86,29 @@ exports.createPost = (req, res, next) => {
 		imageUrl,
 		creator: req.userId,
 	});
-	post.save()
-		.then(result => {
-			console.log(result);
-			return User.findById(req.userId);
-		})
-		.then(user => {
-			creator = user;
-			user.posts.push(post);
-			return user.save();
-		})
-		.then(result => {
-			res.status(201).json({
-				message: 'Post created successfully',
-				post,
-				creator: { _id: creator._id, name: creator.name },
-			});
-		})
-		.catch(err => {
-			if (!err.statusCode) {
-				err.statusCode = 500;
-			}
-			next(err);
+	try {
+		await post.save();
+		const user = await User.findById(req.userId);
+		user.posts.push(post);
+		await user.save();
+		io.getIO().emit('posts', {
+			action: 'create',
+			post: {
+				...post._doc,
+				creator: { _id: req.userId, name: user.name },
+			},
 		});
+		res.status(201).json({
+			message: 'Post created successfully',
+			post,
+			creator: { _id: user._id, name: user.name },
+		});
+	} catch (error) {
+		if (!error.statusCode) {
+			error.statusCode = 500;
+		}
+		next(error);
+	}
 };
 
 /** @type {import('express').RequestHandler} */
--- Social Media - Frontend/src/pages/Feed/Feed.js
+++ Social Media - Frontend/src/pages/Feed/Feed.js
@@ -41,17 +41,26 @@ class Feed extends Component {
 			.catch(this.catchError);
 
 		this.loadPosts();
-		openSocket('http://localhost:8080');
+		const socket = openSocket('http://localhost:8080');
+		socket.on('posts', data => {
+			console.log(data);
+			if (data.action === 'create') {
+				console.log(true);
+				this.addPost(data.post);
+			}
+		});
 	}
 
 	addPost = post => {
 		this.setState(prevState => {
+			console.log(prevState);
 			const updatedPosts = [...prevState.posts];
 			if (prevState.postPage === 1) {
 				if (prevState.posts.length >= 2) {
 					updatedPosts.pop();
 				}
 				updatedPosts.unshift(post);
+				console.log(updatedPosts);
 			}
 			return {
 				posts: updatedPosts,
@@ -188,8 +197,6 @@ class Feed extends Component {
 							p => p._id === prevState.editPost._id
 						);
 						updatedPosts[postIndex] = post;
-					} else if (prevState.posts.length < 2) {
-						updatedPosts = prevState.posts.concat(post);
 					}
 					return {
 						posts: updatedPosts,
