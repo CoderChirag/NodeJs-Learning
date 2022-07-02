85a07cea6c5e1a6d0b07eae69cbe90c13397726c
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-07-03 02:23:38 +0530

    Fix pagination bug

--- Social Media - Frontend/src/pages/Feed/Feed.js
+++ Social Media - Frontend/src/pages/Feed/Feed.js
@@ -275,12 +275,14 @@ class Feed extends Component {
 				};
 				this.setState(prevState => {
 					let updatedPosts = [...prevState.posts];
+					let updatedTotalPosts = prevState.totalPosts;
 					if (prevState.editPost) {
 						const postIndex = prevState.posts.findIndex(
 							p => p._id === prevState.editPost._id
 						);
 						updatedPosts[postIndex] = post;
 					} else {
+						updatedTotalPosts++;
 						if (prevState.posts.length >= 2) {
 							updatedPosts.pop();
 						}
@@ -291,6 +293,7 @@ class Feed extends Component {
 						editLoading: false,
 						isEditing: false,
 						editPost: null,
+						totalPosts: updatedTotalPosts,
 					};
 				});
 			})
