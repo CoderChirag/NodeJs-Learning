b64042c76c815e6739df7b460237cb6e89ac2dc0
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-07-03 20:37:04 +0530

    Type Inference & Type Casting

--- TypeScript-Basics/app.ts
+++ TypeScript-Basics/app.ts
@@ -1,5 +1,14 @@
+const num1Element = document.getElementById('num1') as HTMLInputElement;
+const num2Element = document.getElementById('num2') as HTMLInputElement;
+const buttonElement = document.getElementById('button')!;
+
 function add(num1: number, num2: number) {
 	return num1 + num2;
 }
 
-console.log(add(5, 6));
+buttonElement.addEventListener('click', () => {
+	const num1 = num1Element.value;
+	const num2 = num2Element.value;
+	const result = add(+num1, +num2);
+	console.log(result);
+});
