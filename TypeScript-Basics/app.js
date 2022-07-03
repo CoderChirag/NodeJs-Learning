01ed21739cb39f3a74d96c975b7337cbd6e53f9c
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-07-03 20:29:02 +0530

    Asigning types

--- TypeScript-Basics/app.js
+++ TypeScript-Basics/app.js
@@ -2,4 +2,3 @@ function add(num1, num2) {
     return num1 + num2;
 }
 console.log(add(5, 6));
-console.log(add('5', '6'));
--- TypeScript-Basics/app.ts
+++ TypeScript-Basics/app.ts
@@ -1,6 +1,5 @@
-function add(num1, num2) {
+function add(num1: number, num2: number) {
 	return num1 + num2;
 }
 
 console.log(add(5, 6));
-console.log(add('5', '6'));
