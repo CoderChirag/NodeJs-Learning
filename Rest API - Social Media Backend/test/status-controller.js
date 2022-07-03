d870171a510296321523afdfc9be49c0c0faaf0c
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-07-03 14:45:54 +0530

    Hooks

--- Rest API - Social Media Backend/test/status-controller.js
+++ Rest API - Social Media Backend/test/status-controller.js
@@ -5,10 +5,8 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 const StatusController = require('../controllers/status');
 
-console.log(process.env.MONGO_PWD);
-
 describe('Status Controller - Get Status', () => {
-	it('should send a response with a valid user status for an existing user', done => {
+	before(done => {
 		mongoose
 			.connect(
 				`mongodb+srv://coder:${process.env.MONGO_PWD}@cluster0.iovzfcd.mongodb.net/test-messages?retryWrites=true&w=majority`
@@ -24,24 +22,37 @@ describe('Status Controller - Get Status', () => {
 				return user.save();
 			})
 			.then(() => {
-				const req = { userId: '5e9f8f8f8f8f8f8f8f8f8f8f' };
-				const res = {
-					statusCode: 500,
-					userStatus: null,
-					status: function (code) {
-						this.statusCode = code;
-						return this;
-					},
-					json: function (data) {
-						this.userStatus = data.status;
-					},
-				};
-				StatusController.getStatus(req, res, () => {}).then(() => {
-					expect(res.statusCode).to.equal(200);
-					expect(res.userStatus).to.equal('I am new!');
-					done();
-				});
+				done();
+			});
+	});
+
+	it('should send a response with a valid user status for an existing user', done => {
+		const req = { userId: '5e9f8f8f8f8f8f8f8f8f8f8f' };
+		const res = {
+			statusCode: 500,
+			userStatus: null,
+			status: function (code) {
+				this.statusCode = code;
+				return this;
+			},
+			json: function (data) {
+				this.userStatus = data.status;
+			},
+		};
+		StatusController.getStatus(req, res, () => {}).then(() => {
+			expect(res.statusCode).to.equal(200);
+			expect(res.userStatus).to.equal('I am new!');
+			done();
+		});
+	});
+
+	after(done => {
+		User.deleteMany({})
+			.then(() => {
+				return mongoose.disconnect();
 			})
-			.catch(err => console.log(err));
+			.then(() => {
+				done();
+			});
 	});
 });
