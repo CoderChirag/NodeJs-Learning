6098c2b9ac40cd52b2c5fa02baf7acfd9913fd28
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-07-03 20:43:15 +0530

    Union Types

--- TypeScript-Basics/app.ts
+++ TypeScript-Basics/app.ts
@@ -2,8 +2,13 @@ const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2') as HTMLInputElement;
 const buttonElement = document.getElementById('button')!;
 
-function add(num1: number, num2: number) {
-	return num1 + num2;
+function add(num1: number | string, num2: number | string) {
+	if (typeof num1 === 'number' && typeof num2 === 'number') {
+		return num1 + num2;
+	} else if (typeof num1 === 'string' && typeof num2 === 'string') {
+		return num1 + ' ' + num2;
+	}
+	return +num1 + +num2;
 }
 
 buttonElement.addEventListener('click', () => {
