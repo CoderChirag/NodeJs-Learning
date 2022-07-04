33135b05395e820024f8295998a344bf50d59c11
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-07-04 17:03:35 +0530

    Finish REST Routes

--- TypeScript with Nodejs/routes/todos.ts
+++ TypeScript with Nodejs/routes/todos.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import { Todo } from '../models/todo';
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 const router = Router();
 
@@ -17,6 +17,27 @@ router.post('/todo', (req, res, next) => {
 	};
 
 	todos.push(newTodo);
+	res.status(201).json({ message: 'Todo Added', todo: newTodo, todos });
+});
+
+router.put('/todo/:todoId', (req, res, next) => {
+	const tid = req.params.todoId;
+	const todoIndex = todos.findIndex(todo => todo.id === tid);
+	if (todoIndex === -1) {
+		return res.status(404).json({ error: 'Todo not found' });
+	}
+	todos[todoIndex] = {
+		id: todos[todoIndex].id,
+		text: req.body.text,
+	};
+	return res
+		.status(100)
+		.json({ message: 'Todo Updated', todo: todos[todoIndex], todos });
+});
+
+router.delete('/todo/:todoId', (req, res, next) => {
+	todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+	res.status(200).json({ message: 'Todo Deleted', todos });
 });
 
 export default router;
